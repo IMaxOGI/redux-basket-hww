cc4fff7d81590263f2fe9a8e9f61673fee236cb3
IMaxOGI <max165773@gmail.com>
2021-02-07 16:27:02 +0200

    Update

--- src/shopping_basket_redux_hw/redux/createStore.js
+++ src/shopping_basket_redux_hw/redux/createStore.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from "redux";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import logger from "redux-logger";
 import rootReducer from "./reducers";
 
 export default function () {
-  return createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+  return createStore(
+    rootReducer,
+    composeWithDevTools(applyMiddleware(thunk, logger))
+  );
 }
